4a76ca19cfaabf22fbeeb42e0a81d3c75c8d9b45
Benjamin Atsu <Bredrenbonish@gmail.com>
2024-01-14 17:57:19 -0800

    Filter places by Amenity

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -20,7 +20,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb/', strict_slashes=False)
+@app.route('/4-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -39,7 +39,7 @@ def hbnb():
     # cache_id variable with a UUID
     cache_id = str(uuid.uuid4())
 
-    return render_template('1-hbnb.html',
+    return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places)
